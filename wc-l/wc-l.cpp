e6ef36851271f26df0345ac0818d16a91c384477
Студент <fedyad1997@mail.ru>
2019-04-01 09:37:00 +0900

    task6

--- wc-l/wc-l.cpp
+++ wc-l/wc-l.cpp
@@ -6,40 +6,36 @@
 #include <iomanip> 
 #include <windows.h>
 #include <fstream>
-
-
 using namespace std;
-
+#define N 4096
 int main(int argc, char** argv ) {
     long long t1, t2, freq;
 	long long kol=0;
-	string str;
-	
+	//string str;
+	DWORD nRead;
+	BOOL bResult;
 	//if (argc == 1) 
     //{
     //    cerr << "Error: need text file\n";
     //    return 1;
     //}
-	
 	cout << "Opening file a.txt"; // << argv[1] << "\n";
-	
 	//ifstream f("a.txt");
-	FILE* f;
-
+	HANDLE f = CreateFile(L"a.txt", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	QueryPerformanceFrequency((LARGE_INTEGER *)&freq);// запрашиваем число тиков в 1 сек
-	f = fopen("a.txt", "rb");
 	QueryPerformanceCounter((LARGE_INTEGER *)&t1);// смотрим время после окончания цикла
-	char * s = new char[200000000];
-	while(!feof(f)){
-		fgets(s, 200000000, f);
-		kol++;
+	char str [N];
+	do {
+		bResult=ReadFile(f, str, N, &nRead, NULL);
+		for (int i=0; i<nRead; i++)
+			if (str[i]=='\n')
+				kol++;
 	}
+	while (!(bResult&&nRead==0));
 	QueryPerformanceCounter((LARGE_INTEGER *)&t2);// смотрим время после окончания цикла
-	fclose(f);
-  
+	CloseHandle(f);
 	printf("\nTime spent:%.3f", (t2-t1)/(1.*freq));
 	printf("\n%lld", kol);
-	
 	/*while (!f.eof()) {
 		getline(f, str);
 		kol++;
